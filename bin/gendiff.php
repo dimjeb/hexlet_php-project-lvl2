fad93c6bce39bb09a90f951bd59ab3e0916c3cd0
Dmitriy Zhebrovskiy <dimjeb@gmail.com>
2021-06-21 18:00:53 +0800

    Badge

---
+++ bin/gendiff.php
@@ -0,0 +1,25 @@
+<?php
+require('../vendor/docopt/docopt/src/docopt.php');
+
+$doc = <<<'DOCOPT'
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+DOCOPT;
+
+$result = Docopt::handle($doc, array(
+    'argv'=>array_slice($_SERVER['argv'], 1),
+    'help'=>true,
+    'version'=>null,
+    'optionsFirst'=>false,
+));
+foreach ($result as $k=>$v) {
+    print_r($result);
+    echo $k . ': ' . json_encode($v) . PHP_EOL;
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5b8e9814fa2f567574a607d6c4619733bcc11096',
+    'name' => 'hexlet/code',
+  ),
+  'versions' => 
+  array (
+    'docopt/docopt' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bf3683a16e09fa1665e493eb4d5a29469e132a4f',
+    ),
+    'hexlet/code' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b8e9814fa2f567574a607d6c4619733bcc11096',
+    ),
+  ),
+);
