c964ef811089a74dde8decd13b67252d2ef8a238
Dmitriy Zhebrovskiy <dimjeb@gmail.com>
2021-06-22 09:29:44 +0800

    badge

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInitd85e56adb01dd5f46769a8bde8faa821
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitd85e56adb01dd5f46769a8bde8faa821::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired85e56adb01dd5f46769a8bde8faa821($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequired85e56adb01dd5f46769a8bde8faa821($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
