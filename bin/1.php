6d2a97c9531bae15a06edc6f34e38e74feff7c18
dim <dimjeb@gmail.com>
2021-07-08 14:56:03 +0800

    esc

--- bin/1.php
+++ bin/1.php
@@ -12,7 +12,22 @@ $json2 = '{
   "verbose": true,
   "host": "hexlet.io"
 }';
-$json_1 = json_decode($json1);
-$json_2 = json_decode($json2);
-print_r($json_1);
-// print_r(array_diff($json_1, $json_2));
+$json_1 = json_decode($json1, true);
+$json_2 = json_decode($json2, true);
+foreach ($json_1 as $key => $item) {
+    if (array_key_exists($key, $json_2) && $json_2[$key] == $item) {
+        $result[] = $key . ': ' . $item;
+    } elseif (!array_key_exists($key, $json_2)) {
+        $result[] = '- ' . $key . ': ' . $item;
+    } elseif ($json_2[$key] != $item) {
+        $result[] = '- ' . $key . ': ' . $item;
+        $result[] = '+ ' . $key . ': ' . $json_2[$key];
+    }
+}
+$json2_diff = array_diff_key($json_2, $json_1);
+foreach ($json2_diff as $key => $item) {
+    $result[] = '+ ' . $key . ': ' . $item;
+}
+sort($result);
+
+print_r($result);
